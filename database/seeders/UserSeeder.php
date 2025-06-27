3c148f925a07bc0d43b27cd24c9f5a57c8d1b471
rayan-lagmouch <338041@student.mboutrecht.nl>
2025-06-27 09:29:15 +0200

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Create roles if they don't exist
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $userRole = Role::firstOrCreate(['name' => 'user']);
+
+        // Create admin user
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Admin User',
+                'password' => Hash::make('password'),
+            ]
+        );
+        $admin->assignRole($adminRole);
+
+        // Create 5 demo users
+        User::factory()->count(5)->create()->each(function ($user) use ($userRole) {
+            $user->assignRole($userRole);
+        });
+    }
+}
